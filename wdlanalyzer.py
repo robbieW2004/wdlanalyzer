db0fec845e2c5a2e181939fb2c9a4f41afc47eb4
robbieW2004 <62378933+robbieW2004@users.noreply.github.com>
2021-08-13 13:42:26 +0200

    removed cords
    did an oopsie

--- wdlanalyzer.py
+++ wdlanalyzer.py
@@ -1,7 +1,7 @@
 import anvil
 # pip intsll anvil-parser
-region = anvil.Region.from_file('r.-682.-1229.mca')
-chunk = anvil.Chunk.from_region(region, 17, 6)
+region = anvil.Region.from_file('write file here')
+chunk = anvil.Chunk.from_region(region, 1, 2)
 for z in range(0, 16):
     for y in range(0,128):
         for x in range(0,16):
